69e2c8469c39293fb34dd4ed653d01a7fe40307b
Gabriel Adrian <gabriel.correa2@alunos.sc.senac.br>
2021-09-01 16:45:18 -0300

    Cronograma no menu

--- admin/inc/menu.php
+++ admin/inc/menu.php
@@ -53,7 +53,7 @@ if (isset($_GET['cod_turma'])){
                             <li class="btn list-group-item fundocinza"><a style="text-decoration: none;" class="preto" href="prof.php">Cadastre um Professor</a></li>  -->
                             <li class="btn list-group-item fundocinza"><a style="text-decoration: none;" class="preto" href="disciplina.php?cod_curso=<?php echo $codCurso?>&cod_turma=<?php echo $codTurma ?>">Cadastre uma Disciplina</a></li>
                             <li class="btn list-group-item fundocinza"><a style="text-decoration: none;" class="preto" href="prof.php?cod_curso=<?php echo $codCurso?>&cod_turma=<?php echo $codTurma ?>">Cadastre um Professor</a></li>
-                            <!-- <li class="list-group-item"><a href="turma.php">Cadastre uma Turma</a></li>
+                            <li class="btn list-group-item fundocinza"><a style="text-decoration: none;" class="preto" href="cronograma.php?cod_curso=<?php echo $codCurso?>&cod_turma=<?php echo $codTurma ?>">Cronograma</a></li>
                             <li class="btn list-group-item fundocinza"><a style="text-decoration: none;" class="preto" href="horario.php">Cadastre um Horário</a></li> -->
                         </ul>
 

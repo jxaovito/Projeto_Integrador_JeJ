ebc43f708b2bf642086a1031e2639c55f90c9504
RAFAEL SOARES <rafael.soares1@alunos.sc.senac.br>
2021-09-08 16:22:43 -0300

    Botao cadastrar animacao

--- admin/index.php
+++ admin/index.php
@@ -12,12 +12,18 @@ include_once('inc/cadastros/cadTurma.php');
 
         <div>
             <a href="?logout=true" style="text-decoration: none;" class="btn btn-primary float-start m-2">Sair</a>
+            <div>
+                <a class="bt more-bt float-end" href="#modalCurso" data-bs-toggle="modal">
+                    <span class="fl"></span><span class="sfl"></span><span class="cross"></span><i></i>
+                    <p>Cadastrar curso</p>
+                </a>
+            </div>
             <button type="button" class="btn btn-warning m-2 float-end" data-bs-toggle="modal" data-bs-target="#modalCurso">
                 <h6>Cadastro</h6>
             </button>
             <button type="button" class="btn btn-primary m-2 float-end" data-bs-toggle="modal" data-bs-target="#modalTurma">
-                        Cadastro Turma
-                    </button>
+                Cadastro Turma
+            </button>
         </div>
 
 

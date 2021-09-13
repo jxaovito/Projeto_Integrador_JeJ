7fc73a94fd02bec49faec3251841f64107274eaf
Gabriel Adrian <gabriel.correa2@alunos.sc.senac.br>
2021-09-13 15:45:23 -0300

    Arrumei os cards

--- admin/inc/principal/cards/cardCurso.php
+++ admin/inc/principal/cards/cardCurso.php
@@ -11,7 +11,7 @@ if ($sql->execute()) {
 
 ?>
 
-        <div class="col col-3">
+        <div class="col col-lg-3">
             <div class="card m-2" style="width: 18rem">
 
                 <a onclick="return confirm('Deseja mesmo excluir o curso?');" class='btn btn-outline-danger' href='inc/deletar/delCurso.php?id=<?php echo $cursoCod?>' >Excluir</a>

5d7da9bd2214d77cdd5ec3753e757046644342e4
jxaovito <joaovitoorvieiraborba@gmail.com>
2023-10-17 08:46:25 -0300

    try catch pra descobrir o erro

--- api/usuario/inc/banco.php
+++ api/usuario/inc/banco.php
@@ -34,9 +34,21 @@
   define('DATABASE_USER','u16wsjbvfc7ivhtjw79w');
   define('DATABASE_PASSWORD','pscale_pw_S6cvt7oK7BVE9MkdvbwnVhpEIF6rP5RYTFCPOs8cSuh');
   $options = array(
-    PDO::MYSQL_ATTR_SSL_CA => "C:\Users\joaov\Downloads\ca-bundle.crt",
+    PDO::MYSQL_ATTR_SSL_CA => "C:\Users\joaov\Downloads\ca-bundle.crt"
   );
   
-    $pdo = new PDO('mysql:host='.DATABASE_HOST.';dbname='.DATABASE_NAME.";charset=utf8",DATABASE_USER,DATABASE_PASSWORD, $options);  
+   
   // $pdo = new PDO($dsn, $_ENV["DB_USERNAME"], $_ENV["DB_PASSWORD"], $options);
+  try {
+    $pdo = new PDO('mysql:host='.DATABASE_HOST.';dbname='.DATABASE_NAME.";charset=utf8",DATABASE_USER,DATABASE_PASSWORD, $options);  
+
+    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    echo "Connected successfully"; 
+        var_dump($pdo->query("SHOW STATUS LIKE 'Ssl_cipher';")->fetchAll());
+        $pdo = null;
+}
+catch(PDOException $e)
+    {
+    echo "Connection failed: " . $e->getMessage();
+}
 ?>

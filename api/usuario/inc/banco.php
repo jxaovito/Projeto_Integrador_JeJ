b87593c6f95503e5e21a1e7636b7779f5f1077c7
jxaovito <joaovitoorvieiraborba@gmail.com>
2023-10-17 10:57:57 -0300

    conexão ao banco de volta ao localhost temporariamente

--- api/usuario/inc/banco.php
+++ api/usuario/inc/banco.php
@@ -1,18 +1,16 @@
 <?php
 
     // CONEXÃO AO BANCO DE DADOS
-    // define('DATABASE_HOST','aws.connect.psdb.cloud');
-    // define('DATABASE_NAME','cronograma');
-    // define('DATABASE_USER','u16wsjbvfc7ivhtjw79w');
-    // define('DATABASE_PASSWORD','pscale_pw_S6cvt7oK7BVE9MkdvbwnVhpEIF6rP5RYTFCPOs8cSuh');
-    
-    // $sslOptions = [
-    //     PDO::MYSQL_ATTR_SSL_CA => 'C:\Users\joaov\Downloads\ca-bundle.crt',
-    // ];
+    define('DATABASE_HOST','192.168.0.34:3307');
+    define('DATABASE_NAME','cronograma');
+    define('DATABASE_USER','jej');
+    define('DATABASE_PASSWORD','xxx');
 
-    // $pdo= new PDO('mysql:host='.DATABASE_HOST.';dbname='.DATABASE_NAME.";charset=utf8",DATABASE_USER,DATABASE_PASSWORD);   
+    $pdo= new PDO('mysql:host='.DATABASE_HOST.';dbname='.DATABASE_NAME.";charset=utf8",DATABASE_USER,DATABASE_PASSWORD);   
 ?>
   <?php
+
+  // CONEXÃO AO BANCO DE DADOS DO PLANETSCALE PARA DEPLOY NO VERCEL -- TESTE
 // require_once(__DIR__ . 'vendor/autoload.php');
 
 // use Dotenv\Dotenv;
@@ -29,26 +27,15 @@
 //   $ENV[VERCEL_FORCE_NO_BUILD_CACHE];
 
   // $dsn = "mysql:host=aws.connect.psdb.cloud;dbname=cronograma";
-  define('DATABASE_HOST','aws.connect.psdb.cloud');
-  define('DATABASE_NAME','cronograma');
-  define('DATABASE_USER','u16wsjbvfc7ivhtjw79w');
-  define('DATABASE_PASSWORD','pscale_pw_S6cvt7oK7BVE9MkdvbwnVhpEIF6rP5RYTFCPOs8cSuh');
-  $options = array(
-    PDO::MYSQL_ATTR_SSL_CA => "C:\Users\joaov\Downloads\cacert.pem"
-  );
-  
-   
-  // $pdo = new PDO($dsn, $_ENV["DB_USERNAME"], $_ENV["DB_PASSWORD"], $options);
-  try {
-    $pdo = new PDO('mysql:host='.DATABASE_HOST.';dbname='.DATABASE_NAME.";charset=utf8",DATABASE_USER,DATABASE_PASSWORD, $options);  
+  // define('DATABASE_HOST','aws.connect.psdb.cloud');
+  // define('DATABASE_NAME','cronograma');
+  // define('DATABASE_USER','u16wsjbvfc7ivhtjw79w');
+  // define('DATABASE_PASSWORD','pscale_pw_S6cvt7oK7BVE9MkdvbwnVhpEIF6rP5RYTFCPOs8cSuh');
 
-    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-    echo "Connected successfully"; 
-        var_dump($pdo->query("SHOW STATUS LIKE 'Ssl_cipher';")->fetchAll());
-        $pdo = null;
-}
-catch(PDOException $e)
-    {
-    echo "Connection failed: " . $e->getMessage();
-}
+  // $options = array(
+  //   PDO::MYSQL_ATTR_SSL_CA => "C:\Users\joaov\Downloads\cacert.pem",
+  // );
+  
+  //   $pdo = new PDO('mysql:host='.DATABASE_HOST.';dbname='.DATABASE_NAME.";charset=utf8",DATABASE_USER,DATABASE_PASSWORD, $options);  
+  // $pdo = new PDO($dsn, $_ENV["DB_USERNAME"], $_ENV["DB_PASSWORD"], $options);
 ?>

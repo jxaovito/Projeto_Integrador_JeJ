fc0ece27e44a87080e0b7fc9e01f3d3b9bb4b2ab
jxaovito <joaovitoorvieiraborba@gmail.com>
2023-10-17 10:45:18 -0300

    cacert.pem

--- api/admin/inc/banco.php
+++ api/admin/inc/banco.php
@@ -30,9 +30,9 @@
   define('DATABASE_NAME','cronograma');
   define('DATABASE_USER','u16wsjbvfc7ivhtjw79w');
   define('DATABASE_PASSWORD','pscale_pw_S6cvt7oK7BVE9MkdvbwnVhpEIF6rP5RYTFCPOs8cSuh');
-  
+
   $options = array(
-    PDO::MYSQL_ATTR_SSL_CA => "C:\Users\joaov\Downloads\ca-bundle.crt",
+    PDO::MYSQL_ATTR_SSL_CA => "C:\Users\joaov\Downloads\cacert.pem",
   );
   
     $pdo = new PDO('mysql:host='.DATABASE_HOST.';dbname='.DATABASE_NAME.";charset=utf8",DATABASE_USER,DATABASE_PASSWORD, $options);  
--- api/usuario/inc/banco.php
+++ api/usuario/inc/banco.php
@@ -34,7 +34,7 @@
   define('DATABASE_USER','u16wsjbvfc7ivhtjw79w');
   define('DATABASE_PASSWORD','pscale_pw_S6cvt7oK7BVE9MkdvbwnVhpEIF6rP5RYTFCPOs8cSuh');
   $options = array(
-    PDO::MYSQL_ATTR_SSL_CA => "C:\Users\joaov\Downloads\ca-bundle.crt"
+    PDO::MYSQL_ATTR_SSL_CA => "C:\Users\joaov\Downloads\cacert.pem"
   );
   
    

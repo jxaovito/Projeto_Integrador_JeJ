9c88946294082a0c3683ff18f3794a6acadc3de4
jxaovito <joaovitoorvieiraborba@gmail.com>
2023-11-23 09:24:39 -0300

    conexão ao banco de dados atualizada, dessa vez usando mysqli para efetuar a conexão

--- api/banco/banco.php
+++ api/banco/banco.php
@@ -16,21 +16,37 @@ include __DIR__."/../../vendor/autoload.php";
 $dotenv = Dotenv\Dotenv::createImmutable(dirname(__DIR__));
 $dotenv->safeLoad();
 
-// $hostname = getenv('DB_HOST');
-// $dbName = getenv('DB_NAME');
-// $username = getenv('DB_USERNAME');
-// $password = getenv('DB_PASSWORD');
-// $dsn = getenv('MYSQL_ATTR_SSL_CA');
-// $options = array(
-// 	PDO::MYSQL_ATTR_SSL_CA => 'C:\Users\joaov\Downloads\cacert-2023-08-22.pem',
-// );
+// // $hostname = getenv('DB_HOST');
+// // $dbName = getenv('DB_NAME');
+// // $username = getenv('DB_USERNAME');
+// // $password = getenv('DB_PASSWORD');
+// // $dsn = getenv('MYSQL_ATTR_SSL_CA');
+// // $options = array(
+// // 	PDO::MYSQL_ATTR_SSL_CA => 'C:\Users\joaov\Downloads\cacert-2023-08-22.pem',
+// // );
 
-var_dump($_ENV['DB_HOST']);
-var_dump($_ENV['DB_NAME']);
-var_dump($_ENV['DB_PASSWORD']);
-var_dump($_ENV['DB_USERNAME']);
+// var_dump($_ENV['DB_HOST']);
+// var_dump($_ENV['DB_NAME']);
+// var_dump($_ENV['DB_PASSWORD']);
+// var_dump($_ENV['DB_USERNAME']);
 
-    $pdo = new PDO('mysql:host='.getenv('DB_HOST').';dbname='.getenv('DB_NAME').";charset=utf8",getenv('DB_USERNAME'),getenv('DB_PASSWORD'),getenv('MYSQL_ATTR_SSL_CA'));   
+//     $pdo = new PDO(getenv('DB_HOST'),getenv('DB_NAME'),getenv('DB_USERNAME'),getenv('DB_PASSWORD'),getenv('MYSQL_ATTR_SSL_CA'));   
+//
+
+$hostname = $_ENV['DB_HOST'];
+$dbName = $_ENV['DB_NAME'];
+$username = $_ENV['DB_USERNAME'];
+$password = $_ENV['DB_PASSWORD'];
+$ssl = $_ENV['MYSQL_ATTR_SSL_CA'];
 
 
-?>
+$mysqli = mysqli_init();
+$mysqli->ssl_set(NULL, NULL, $ssl, NULL, NULL);
+$mysqli->real_connect($hostname, $username, $password, $dbName);
+
+
+if ($mysqli->connect_error) {
+    echo 'Erro ao conectar no banco de dados';
+} else {
+    echo "Conectado com sucesso";
+}?>
